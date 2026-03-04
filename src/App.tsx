5813a9eb16bcd1d3d8805f452e9329d602b70afa
Kumili Thanusri <thanusrikumili91@gmail.com>
2026-03-04 13:50:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,23 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
 import Landing from "./pages/Landing";
-import Login from "./pages/Login";
-import StudentDetails from "./pages/StudentDetails";
 import ResumeUpload from "./pages/ResumeUpload";
-import Mapping from "./pages/Mapping";
 import JobListings from "./pages/JobListings";
+import Layout from "./components/Layout";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Landing />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/details" element={<StudentDetails />} />
-            <Route path="/upload" element={<ResumeUpload />} />
-            <Route path="/mapping" element={<Mapping />} />
-            <Route path="/jobs" element={<JobListings />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Landing />} />
+          <Route path="/upload" element={<ResumeUpload />} />
+          <Route path="/jobs" element={<JobListings />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
