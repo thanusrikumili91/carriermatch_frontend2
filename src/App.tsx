926bab8dc43e65e7d580f930cb40ff85f2a9c7b5
Kumili Thanusri <thanusrikumili91@gmail.com>
2026-03-04 14:11:52 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
-import ResumeUpload from "./pages/ResumeUpload";
-import JobListings from "./pages/JobListings";
+
 import Layout from "./components/Layout";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import StudentDetails from "./pages/StudentDetails";
+import ResumeUpload from "./pages/ResumeUpload";
+import Mapping from "./pages/Mapping";
+import JobListings from "./pages/JobListings";
 import NotFound from "./pages/NotFound";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Landing />} />
-          <Route path="/upload" element={<ResumeUpload />} />
-          <Route path="/jobs" element={<JobListings />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          {/* Layout wraps all main pages */}
+          <Route element={<Layout />}>
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/details" element={<StudentDetails />} />
+            <Route path="/upload" element={<ResumeUpload />} />
+            <Route path="/mapping" element={<Mapping />} />
+            <Route path="/jobs" element={<JobListings />} />
+          </Route>
+
+          {/* Catch-all for 404 */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
